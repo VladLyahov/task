310cf8df65e4238bce62dbb7306f2b5d6848f42e
VladLyahov <vlad-lyahov@mail.ru>
2016-06-21 22:57:44 +0300

    plus to user input

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,51 @@
+import static java.lang.Math.pow;
+
+/**
+ * Created by vlad on 21.06.2016.
+ */
 public class Calculator {
     /**
      *
      */
-    private int result;
+    private double result;
     /**
      *
      */
-    public void add(int ... params){
-        for (Integer param:params){
+    public void add(double ... params){
+        for (Double param:params){
             this.result +=param;
         }
+    }
+
+    public void subtraction (double ... params){
+        this.result = 2*params[0];
+        for (Double param:params){
+            this.result-=param;
+        }
+    }
+
+    public void multiplication (double ... params){
+        this.result = 1;
+        for (Double param:params){
+            this.result *= param;
+        }
+    }
+
+    public void division(double ... params){
+        this.result = params[0]*params[0];
+        for (Double param:params){
+            this.result /= param;
+        }
+    }
+
+    public void involution (double ... params) {
+        this.result = pow( params[0], params[1]);
     }
 
     /*
 
      */
-    public int getResult(){
+    public double getResult(){
         return this.result;
     }
     /*
--- InteractRunner.java
+++ InteractRunner.java
@@ -8,16 +8,36 @@ public class InteractRunner {
         try{
             Calculator calc = new Calculator();
             String exit = "no";
+            String saveResult  = "no";
             while(!exit.equals("yes")){
-                System.out.println("Enter first arg : ");
-                String first = reader.next();
+                String first;
+                if (!saveResult.equals("yes")) {
+                    calc.cleanResult();
+                    System.out.println("Enter first arg : ");
+                     first = reader.next();
+                } else {
+                    first = Double.toString(calc.getResult());
+                }
+
                 System.out.println("Enter second arg : ");
                 String second = reader.next();
-                calc.add(Integer.valueOf(first),Integer.valueOf(second));
+
+                System.out.println("type of manipulation : '+' '-' '*' '/' '^' ");
+                String manipulation = reader.next();
+                if (manipulation.equals("+")) calc.add(Double.valueOf(first),Double.valueOf(second));
+                else if (manipulation.equals("-")) calc.subtraction(Double.valueOf(first),Double.valueOf(second));
+                else if (manipulation.equals("*")) calc.multiplication(Double.valueOf(first),Double.valueOf(second));
+                else if (manipulation.equals("/")) calc.division(Double.valueOf(first),Double.valueOf(second));
+                else if (manipulation.equals("^")) calc.involution(Double.valueOf(first),Double.valueOf(second));
+                else System.out.println("error");
+
+
                 System.out.println("REsult : " + calc.getResult());
-                calc.cleanResult();
+
                 System.out.println("exit : yes/no");
                 exit = reader.next();
+                System.out.println("save result : yes/no");
+                saveResult = reader.next();
             }
         } finally {
             reader.close();
